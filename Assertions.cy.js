697330526bc20bbe4c2e8a2ed390903e38dd3d06
Roldán Francisco <98569621+roldn@users.noreply.github.com>
2023-04-17 17:01:23 -0300

    Explicit assertions added

--- Assertions.cy.js
+++ Assertions.cy.js
@@ -14,22 +14,38 @@ describe('XPathLocators', () => {
     .and('not.eq', 'xxxxxxx')
     .and('not.contain', 'xxxxxxxxx')
   
-  })
+    cy.title()
+    .should('include', 'Orange')
+    .and('eq', 'OrangeHRM')
+    .and('contain', 'HRM')
 
+    cy.get('.orangehrm-login-branding > img').should('exist').and('be.visible')
+
+    cy.xpath("//a").should('have.length', '5') //number of links
+
+    cy.get("input[placeholder='Username']").type("Admin")
+    cy.get("input[placeholder='Username']").should('have.value', 'Admin')
+  })
+  
   it('Explicit assertions', () => {
   
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
   
-    cy.url()
-    .should('include', 'orangehrmlive.com')
-    .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    .and('contain', 'orangehrm')
-  
+    cy.get("input[placeholder='Username']").type("Admin")
+    cy.get("input[placeholder='Password']").type("admin123")
+    cy.get("button[type='submit']").click()
 
-    cy.url()
-    .should('not.include', 'xxxxxx')
-    .and('not.eq', 'xxxxxxx')
-    .and('not.contain', 'xxxxxxxxx')
-  
+    let expName="xyz"
+    cy.get(".oxd-userdropdown-name").then((x)=>{
+      let actName = x.text()
+
+      //BDD Style
+      chai.expect(actName).to.equal(expName)
+      chai.expect(actName).to.not.equal(expName)
+
+      //TDD Style
+      chai.assert.equal(actName,expName)
+      chai.assert.notEqual(actName,expName)
+    })
   })
 })
